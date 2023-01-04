bf16348f654119ca9e40de571faad6fa1b665690
t0in4 <t0in4@yahoo.com>
2023-01-04 09:23:02 +0300

    Дописать решение

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+	public static void class main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		// дописать код
+		int size = ввод размера массива;
+		// объявили массив с заданным размером
+		int[] myArray = new int[size]; // int[size] переменная которую инициализировали в предыдущей строке
+        	for (int i = 0; i < size; i++) {
+            		myArray[i] = scanner.nextInt();
+        	}
+		ввод числа сравнения;
+		декларация переменной для суммы;
+		for (int i = 0; i < myArray.length; i++) {
+			if (i < (меньше чем число сравнения) {
+				(переменная суммы)++
+			}
+		}
+		System.out.println( переменная суммы );
+	}
+}
